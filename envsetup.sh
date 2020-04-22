2cbdb4df0c89fd87a0bf4bcc2cd85077b2836c6a
fogwizard <jwf1990@126.com>
2020-04-22 17:06:12 +0800

    update

--- envsetup.sh
+++ envsetup.sh
@@ -1,10 +1,13 @@
 #!bash
 cross_compile=`readlink -f ~/bin/gcc-linaro-7.2.1-2017.11-x86_64_arm-linux-gnueabihf/bin`
+current_path=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+
+echo buildscript_path=${current_path}
 echo cross_compile_compile=${cross_compile}
 export PATH=$PATH:${cross_compile}
 
 function croot(){
-	return
+	cd ${current_path}/../
 }
 
 function build_stressapptest(){

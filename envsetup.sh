5edcdff05fdbd0f75761549d304133ba78f7be6e
fogwizard <jwf1990@126.com>
2020-04-22 16:54:03 +0800

    Add envsetup

---
+++ envsetup.sh
@@ -0,0 +1,14 @@
+#!bash
+cross_compile=`readlink -f ~/bin/gcc-linaro-7.2.1-2017.11-x86_64_arm-linux-gnueabihf/bin`
+echo cross_compile_compile=${cross_compile}
+export PATH=$PATH:${cross_compile}
+
+function croot() {
+}
+
+function build_stressapptest() {
+	croot
+	cd stressapptest
+	./configure --host=arm-linux-gnueabihf  --with-static
+	make
+}
